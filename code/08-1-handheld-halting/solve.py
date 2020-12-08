fa1020266ce08f226dad6c5f2ca8e46386a14712
Pavlo Ivashkov <pavels.code@gmail.com>
2020-12-08 07:41:41 +0200

    day 8

---
+++ code/08-1-handheld-halting/solve.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python -OO
+import aocpaiv as aoc
+import re
+
+
+def run_program(program):
+    acc = 0
+    ip = 0
+    seen = set()
+    while 0 <= ip < len(program):
+        if ip in seen: break
+        seen.add(ip)
+        op, x = program[ip]
+        aoc.trace(ip, op, x)
+
+        if op == 'acc':
+            acc += x
+            ip += 1
+        elif op == 'jmp':
+            ip += x
+        elif op == 'nop':
+            ip += 1
+    return acc
+
+
+def solve(text):
+    program = [(n, int(x)) for s in text.strip().splitlines() for n,x in [s.split()]]
+    aoc.trace(program)
+    return run_program(program)
+
+
+def test():
+    aoc.test_subject(solve)
+    aoc.test("""
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+""") == 5
+
+
+if __name__ == '__main__':
+    test()
+    print(solve(aoc.read_files()))
---
+++ code/08-2-handheld-halting/solve.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python -OO
+import aocpaiv as aoc
+import re
+
+
+def run_program(program):
+    acc = 0
+    ip = 0
+    seen = set()
+    while 0 <= ip < len(program):
+        if ip in seen: return
+        seen.add(ip)
+        op, x = program[ip]
+
+        if op == 'acc':
+            acc += x
+            ip += 1
+        elif op == 'jmp':
+            ip += x
+        elif op == 'nop':
+            ip += 1
+    return acc
+
+
+def solve(text):
+    program = [(n, int(x)) for s in text.strip().splitlines() for n,x in [s.split()]]
+    patches = ((i, ('nop' if op == 'jmp' else 'jmp'), x) for i, (op, x) in enumerate(program) if op in ('jmp', 'nop'))
+
+    mem = list(program)
+    while (acc := run_program(mem)) is None:
+        mem = list(program)
+        i, op, x = next(patches)
+        mem[i] = (op, x)
+    aoc.trace('patch', i, op, x)
+    return acc
+
+
+def test():
+    aoc.test_subject(solve)
+    aoc.test("""
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+""") == 8
+
+
+if __name__ == '__main__':
+    test()
+    print(solve(aoc.read_files()))
